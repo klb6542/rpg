809989e2ab68b3ec067fb914ccf35c00f023bec5
klb6542 <keeganthebates2023@gmail.com>
2024-02-15 23:06:30 -0500

    commit

--- rpg/main.cpp
+++ rpg/main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <SFML/OpenGL.hpp>
+#include <SFML/Audio.hpp>
+#include "SFML/Graphics.hpp"
 
 int main() {
-	std::cout << "Hello world!" << std::endl;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "RPG");
 
-	return 0;
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+                std::cout << "Pressed a on keyboard" << std::endl;
+            }
+
+            window.clear();
+            sf::CircleShape square(80.f, 4);
+            window.draw(square);
+            window.display();
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
